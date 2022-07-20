cbf9aeeec8086c5df6b39e8e2a7bd6f8d947f5a0
vinhphucit <vinhphuc.it@gmail.com>
2022-07-21 06:26:32 +0700

    chore(*): clean up build

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/Objects-normal/arm64/path_provider_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/phuc.tran/Workplace/IDEA/EZLanguage/EZLanguage-FE-FlashCard-Flutter/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-dummy.m \
-  /Users/phuc.tran/Workplace/IDEA/EZLanguage/EZLanguage-FE-FlashCard-Flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/module.modulemap \
-  /Users/phuc.tran/Workplace/IDEA/EZLanguage/EZLanguage-FE-FlashCard-Flutter/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-prefix.pch
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/window_size.build/Objects-normal/arm64/window_size-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/phuc.tran/Workplace/IDEA/EZLanguage/EZLanguage-FE-FlashCard-Flutter/macos/Pods/Target\ Support\ Files/window_size/window_size-dummy.m \
-  /Users/phuc.tran/Workplace/IDEA/EZLanguage/EZLanguage-FE-FlashCard-Flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/window_size.build/module.modulemap \
-  /Users/phuc.tran/Workplace/IDEA/EZLanguage/EZLanguage-FE-FlashCard-Flutter/macos/Pods/Target\ Support\ Files/window_size/window_size-prefix.pch
